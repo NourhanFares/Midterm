c2280fb02d1775b899836145a8045f6049e46f8e
NourhanFares <faresnour28@gmail.com>
2023-08-21 22:24:20 +0300

    display stat

--- main.py
+++ main.py
@@ -20,10 +20,36 @@ input_username = 0
 input_pass = 0
 
 
+def admin_menu():
+  print("Enter:")
+  print("1. Display Statistics")
+  print("2. Add an Employee")
+  print("3. Display all Employees")
+  print("4. Change Employee’s Salary")
+  print("5. Remove Employee")                           #run time o(1)
+  print("6. Raise Employee’s Salary")
+  print("7. Exit")
+
+males = 0
+females = 0
+
+
+def display_statistics(employees_info):
+  males = 0
+  females = 0
+
+  for emp_id, data in employees_info.items():
+                                                        #run time: o(N) n is nb of employees
+    if data ['gender'] == 'male':
+      males += 1
+    elif data['gender'] == 'female':
+      females += 1
+  print("Number of male employees is:", males)
+  print("Number of female employees is:", females)
 
 
 
-
+  
     
 
 
